48880d14e2024481d92d9e6bccc30b464fb54c68
AndresCortes <fluch001@gmail.com>
2020-06-06 23:51:56 -0500

    adding for exercises
    Ejercicio 3.

--- src/com/fluch/ClasesModulo2/Main.java
+++ src/com/fluch/ClasesModulo2/Main.java
@@ -3,8 +3,8 @@ package com.fluch.ClasesModulo2;
 public class Main {
 
     public static void main(String[] args) {
-	//condicionalesSimples.Ejercicio1();
-	//condicionalesSimples.Ejercicio2();
+	    //condicionalesSimples.Ejercicio1();
+	    //condicionalesSimples.Ejercicio2();
     	//condicionalesSimples.Ejercicio3();
         //condicionalesSimples.Ejercicio4();
         //condicionalesSimples.Ejercicio5();
@@ -14,7 +14,9 @@ public class Main {
         //condicionalesComplejos.Ejercicio4();
         //switchCase.Ejercicio1();
         //switchCase.Ejercicio2();
-        forEjercicios.Ejercicio1();
+        //forEjercicios.Ejercicio1();
+        //forEjercicios.Ejercicio2();
+        forEjercicios.Ejercicio3();
 
     }
 }
--- src/com/fluch/ClasesModulo2/forEjercicios.java
+++ src/com/fluch/ClasesModulo2/forEjercicios.java
@@ -14,4 +14,36 @@ public class forEjercicios {
         System.out.println("El factorial de " + numero + " es: " + factorial);
         myObj.close();
     }//end Ejercicio1
+    public static void Ejercicio2(){
+        //Desarrollar un programa que determine los divisores de un número definido previamente.
+        int divisor, count=0;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Ingrese el numero que desee que sea evaluado:");
+        int numero = myObj.nextInt();
+        for (int i=1;i<numero;i++){
+            divisor=numero%i;
+            if (divisor==0)
+            count++;
+        }//end for
+        System.out.println("El numero " + numero + " tiene: " + count + " divisores");
+        myObj.close();
+    }//end Ejercicio2
+    public static void Ejercicio3(){
+        // Ejercicio3: Desarrollar un programa que permita definir si un número entero
+        // ya establecido es un número perfecto o no.
+        int divisor, suma=0;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Ingrese el numero que desee que sea evaluado:");
+        int numero = myObj.nextInt();
+        for (int i=1;i<numero;i++){
+            divisor=numero%i;
+            if (divisor==0)
+                suma=i+suma;    
+        }//end for
+        if (suma==numero)
+            System.out.println("El numero " + numero + " es perfecto. ");
+        else
+            System.out.println("El numero " + numero + " no es perfecto. ");
+        myObj.close();
+    }//end Ejercicio3
 }//end class
